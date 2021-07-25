898934f20b5ae9ab4c196256154c3b68317b58e9
MarconLP <13001502+MarconLP@users.noreply.github.com>
2021-07-25 12:13:19 +0200

    add route to reorder taskgroups

--- routes/taskgroups.js
+++ routes/taskgroups.js
@@ -13,6 +13,18 @@ router.post('/:boardId', async (req, res) => {
     res.send('OK')
 })
 
+router.patch('/:boardId', async (req, res) => {
+    const { boardId } = req.params
+    const { result } = req.body
+    const board = await Board.findById(boardId)
+
+    const [taskgroup] = board.taskGroups.splice(result.source.index, 1)
+    board.taskGroups.splice(result.destination.index, 0, taskgroup)
+
+    await board.save()
+    res.send('OK')
+})
+
 router.delete('/:boardId/:taskGroupId', async (req, res) => {
     const { boardId, taskGroupId } = req.params
     const board = await Board.findById(boardId)
