50670146a1a92565aa1ea8ad87569ba708d25d89
MarconLP <13001502+MarconLP@users.noreply.github.com>
2021-11-21 13:42:36 +0100

    use .env variables

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(
     session({
-        secret: "secretcode",
+        secret: process.env.COOKIE_SECRET,
         resave: true,
         saveUninitialized: true,
         store: new MongoDBStore({
@@ -37,13 +37,13 @@ app.use(
         })
     })
 )
-app.use(cookieParser("secretcode"))
+app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(passport.initialize())
 app.use(passport.session())
 require("./passportConfig")(passport)
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://192.168.0.239:3000', 'https://web.postman.co'],
+    origin: [process.env.FRONTEND_HOST],
     credentials: true
 }))
 
@@ -55,49 +55,6 @@ app.use('/api/taskgroup', taskGroups)
 app.use('/api/task', tasks)
 app.use('/api/attribute', attributes)
 
-// app.get('/api/seeds', async (req, res) => {
-//     await Workspaces.deleteMany({})
-//     await Boards.deleteMany({})
-//
-//     const task = new Tasks({
-//         name: 'fix game not ending after timer ends',
-//         options: [{name: 'status', value: 0}, {name:'deployed', value:0}]
-//     })
-//     const taskGroup = new TaskGroups({
-//         name: 'Ingame state',
-//         tasks: [task]
-//     })
-//     const board = new Boards({
-//         name: 'TTT',
-//         members: ['game'],
-//         attributes: [
-//             {
-//                 name: 'status',
-//                 labels: [{name: 'stuck', color:'#E2445C'}, {name: 'assigned', color: '#F9D900'}, {name: 'open', color: '#667684'}]
-//             },
-//             {
-//                 name: 'deployed',
-//                 labels: [{name: 'testing', color:'#E2445C'}, {name: 'yes', color: '#2ECD6F'}]
-//             }
-//         ],
-//         taskGroups: [taskGroup]
-//     })
-//     const space = new Spaces({
-//         name: 'Server Project',
-//         boards: [board]
-//     })
-//     const workspace = new Workspaces({
-//         id: 9719,
-//         owner: 'game',
-//         spaces: [space]
-//     })
-//
-//     await workspace.save()
-//     await board.save()
-//
-//     res.send('test')
-// })
-
-app.listen(3001, () => {
-    console.log('Listening on port 3001')
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Listening on port ${process.env.PORT || 3001}`)
 })
