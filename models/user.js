a9181e28874ea8aa0965c10b8973453606510295
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-09-03 16:52:37 +0200

    add notifications data to user model

--- models/user.js
+++ models/user.js
@@ -1,26 +1,51 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  workspaces: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Workspace",
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    workspaces: [
+  ],
+  email: {
+    type: String,
+    required: true,
+    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+  },
+  notifications: [
+    {
+      workspaceId: Number,
+      new: [
         {
-            type: Schema.Types.ObjectId,
-            ref: 'Workspace'
-        }
-    ],
-    email: {
-        type: String,
-        required: true,
-        match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
-    }
-})
+          taskId: String,
+          location: {
+            space: String,
+            board: String,
+          },
+          changes: [],
+        },
+      ],
+      cleared: [
+        {
+          taskId: String,
+          location: {
+            space: String,
+            board: String,
+          },
+          changes: [],
+        },
+      ],
+    },
+  ],
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
--- passportConfig.js
+++ passportConfig.js
@@ -3,42 +3,43 @@ const bcrypt = require("bcrypt");
 const LocalStrategy = require("passport-local").Strategy;
 
 module.exports = function (passport) {
-    passport.use(
-        new LocalStrategy((username, password, done) => {
-            User.findOne({ username: username }, (err, user) => {
-                if (err) throw err;
-                if (!user) return done(null, false);
-                bcrypt.compare(password, user.password, (err, result) => {
-                    if (err) throw err;
-                    if (result === true) {
-                        return done(null, user);
-                    } else {
-                        return done(null, false);
-                    }
-                })
-            }).populate({
-                path: 'workspaces',
-                model: 'Workspace',
-                select: ['id', 'name']
-            })
-        })
-    )
-
-    passport.serializeUser((user, cb) => {
-        cb(null, user.id);
-    });
-    passport.deserializeUser((id, cb) => {
-        User.findOne({ _id: id }, (err, user) => {
-            const userInformation = {
-                username: user.username,
-                workspaces: user.workspaces,
-                _id: user._id,
-            }
-            cb(err, userInformation);
-        }).populate({
-            path: 'workspaces',
-            model: 'Workspace',
-            select: ['id', 'name']
-        })
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err) throw err;
+          if (result === true) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        });
+      }).populate({
+        path: "workspaces",
+        model: "Workspace",
+        select: ["id", "name"],
+      });
     })
-}
+  );
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+  });
+  passport.deserializeUser((id, cb) => {
+    User.findOne({ _id: id }, (err, user) => {
+      const userInformation = {
+        username: user.username,
+        workspaces: user.workspaces,
+        _id: user._id,
+        notifications: user.notifications,
+      };
+      cb(err, userInformation);
+    }).populate({
+      path: "workspaces",
+      model: "Workspace",
+      select: ["id", "name"],
+    });
+  });
+};
