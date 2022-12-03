c12c2a04bdb85cd85ef6d4e6481bbe07b2efa039
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-12-03 23:38:32 +0100

    cleanup database when deleting workspace

--- models/space.js
+++ models/space.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Board = require('./board')
 const Schema = mongoose.Schema
 
 const spaceSchema = new mongoose.Schema({
@@ -16,6 +17,14 @@ const spaceSchema = new mongoose.Schema({
         type: Schema.Types.ObjectId,
         ref: 'Workspace'
     }
+})
+
+spaceSchema.post('findOneAndDelete', async function (space) {
+    if (!space) return
+
+    space.boards.map(async board => {
+        await Board.findByIdAndDelete(board)
+    })
 })
 
 module.exports = mongoose.model('Space', spaceSchema)
