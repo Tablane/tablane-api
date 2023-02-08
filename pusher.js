0e0617de00993feeba785553cb38e92b7cb30380
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-02-08 21:37:29 +0100

    switch to soketi.app

--- pusher.js
+++ pusher.js
@@ -3,10 +3,11 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 const pusher = new Pusher({
+    host: process.env.PUSHER_HOST,
+    useTLS: true,
     appId: process.env.PUSHER_APP,
     key: process.env.PUSHER_KEY,
-    secret: process.env.PUSHER_SECRET,
-    cluster: process.env.PUSHER_CLUSTER
+    secret: process.env.PUSHER_SECRET
 })
 
 module.exports = pusher
