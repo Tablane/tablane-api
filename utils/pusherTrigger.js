1d793db6ea06fe5b141e1d83f633905a6ab4c3ab
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-02-08 18:26:34 +0100

    switch from socketio to pusher

---
+++ utils/pusherTrigger.js
@@ -0,0 +1,18 @@
+const pusher = require('../pusher')
+exports.pusherTrigger = ({ req, boardId, workspaceId, event, body }) => {
+    if (boardId) {
+        pusher.trigger('private-board-' + boardId, 'updates', {
+            event,
+            id: boardId,
+            sessionId: req.headers['pusher-session'],
+            body
+        })
+    } else if (workspaceId) {
+        pusher.trigger('private-workspace-' + workspaceId, 'updates', {
+            event,
+            id: workspaceId,
+            sessionId: req.headers['pusher-session'],
+            body
+        })
+    }
+}
